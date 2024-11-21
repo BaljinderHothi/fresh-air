fc789d5723f294deae99988bcb7eabb343af0ae8
BaljinderHothi <BaljinderH.cs@gmail.com>
2024-11-21 11:33:31 -0500

    adding more css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen">
-      <aside className="w-1/5 bg-gray-100 p-4">
-        <nav className="flex flex-col gap-4">
-          <a href="/">Main Page</a>
-          <a href="/Lungs">Lungs</a>
-          <a href="/Pneumonia">Pneumonia</a>
-          <a href="/Model">Model</a>
-          <a href="/Quiz">Quiz</a>
-        </nav>
-      </aside>
-      <main className="flex-grow p-6">{children}</main>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="flex min-h-screen">
+          {/* Side Navigation */}
+          <aside className="w-1/5 bg-gray-100 p-4">
+            <nav className="flex flex-col gap-4">
+              <a href="/">Main Page</a>
+              <a href="/Lungs">Lungs</a>
+              <a href="/Pneumonia">Pneumonia</a>
+              <a href="/Model">Model</a>
+              <a href="/Quiz">Quiz</a>
+            </nav>
+          </aside>
+
+          {/* Main Content */}
+          <main className="flex-grow p-6">{children}</main>
+        </div>
+      </body>
+    </html>
   );
 }
